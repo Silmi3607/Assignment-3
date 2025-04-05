aa02a1a4de7eca77f6937f4eae2e0bd5b4a7f375
Silmi3607 <123944620+Silmi3607@users.noreply.github.com>
2025-04-05 18:21:47 +0500

    Update send_report_email.sh

--- send_report_email.sh
+++ send_report_email.sh
@@ -2,4 +2,4 @@
 echo "Sending email with test and dependency-check reports..."
 # Example placeholder script (requires mailx or sendemail tool)
 # sudo apt install mailutils -y
-echo "DevSecOps Report" | mail -s "Pipeline Report" -A unit-test-report.txt -A reports/dependency-check-report.html syedmeesumalimeer@yahoo.com
+echo "DevSecOps Report" | mail -s "Pipeline Report" -A unit-test-report.txt -A reports/dependency-check-report.html k213607@nu.edu.pk
